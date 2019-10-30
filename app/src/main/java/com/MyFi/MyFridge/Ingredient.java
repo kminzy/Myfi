1a5c59fc42961f95aeb44cde6a085839a048d0ad
yoonh-j <56199009+yoonh-j@users.noreply.github.com>
2019-10-30 19:36:06 +0900

    - 재료 이름 입력 시 자동완성 기능 추가 - 재료 객체에 추가할 재료 이름, 식품유형, 보관장소 저장 - IngredientData로 대체되는 Ingredient의 클래스 삭제

--- app/src/main/java/com/MyFi/MyFridge/Ingredient.java
+++ app/src/main/java/com/MyFi/MyFridge/Ingredient.java
@@ -1,6 +1,5 @@
 package com.MyFi.MyFridge;
 
-//TODO:클래스 삭제 필요 IngredientData로 대체
 public class Ingredient {
 
     private String ingredientName;
@@ -11,17 +10,5 @@ public class Ingredient {
         this.ingredientName = ingredientName;
         this.dDay = dDay;
         this.expirationDate = expirationDate;
-    }
-
-    public String getIngredientName() {
-        return ingredientName;
-    }
-
-    public String getDDay() {
-        return dDay;
-    }
-
-    public String getExpirationDate() {
-        return expirationDate;
     }
 }
