a592c33e38d5c82642981aef53f4319332cbc339
ByoungHwi <48614208+ByoungHwi@users.noreply.github.com>
2019-11-09 16:17:30 +0900

    addHate button

--- app/src/main/java/com/MyFi/MyFridge/ViewRecipeActivity.java
+++ app/src/main/java/com/MyFi/MyFridge/ViewRecipeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +12,23 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.MyFi.MyFridge.domain.dto.RecipeDto;
+import com.MyFi.MyFridge.domain.entitiy.User;
+import com.MyFi.MyFridge.httpConnect.HttpConnection;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 public class ViewRecipeActivity extends AppCompatActivity {
+
+    private HttpConnection httpConn = HttpConnection.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,5 +62,40 @@ public class ViewRecipeActivity extends AppCompatActivity {
 
             }
         });
+
+        Button hateButton = findViewById(R.id.hate_button);
+        hateButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                addHatedUser(((MainActivity)MainActivity.mContext).recipeDto.getName());
+
+            }
+        });
+
+
+
     }
+
+    public final void addHatedUser(final String recipeName) {
+        new Thread() {
+            public void run() {
+                SharedPreferences sharedPreferences = getSharedPreferences("uid",MODE_PRIVATE);
+                ((MainActivity)MainActivity.mContext).user.setUid(sharedPreferences.getInt("uid",1));
+                httpConn.addHatedUser(((MainActivity)MainActivity.mContext).user, recipeName, addHatedCallback);
+            }
+        }.start();
+    }
+
+    public final Callback addHatedCallback = new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
+        }
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+
+        }
+    };
+
+
 }
